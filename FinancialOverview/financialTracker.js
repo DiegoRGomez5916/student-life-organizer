a05ee634694edf34e38bbc2a14cd34f3c97d896e
lcontreras18 <123529844+lcontreras18@users.noreply.github.com>
2026-03-16 14:41:53 -0700

    Update financialTracker.js
    added method addTransaction - asks user for transaction name and amount. Not functional yet

--- FinancialOverview/financialTracker.js
+++ FinancialOverview/financialTracker.js
@@ -1,3 +1,9 @@
 class FinancialTraker{
+  addTransaction() {
+    const name = prompt("Please enter transaction name:");
+    const amount = prompt("Please enter transaction amount:");
 
+    if(!name || amount) return;
+
+  }
 }
