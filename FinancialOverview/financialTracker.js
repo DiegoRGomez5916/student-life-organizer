4eac0e2574a46526804b9b24f10f5c23ce588f18
lcontreras18 <123529844+lcontreras18@users.noreply.github.com>
2026-03-21 18:29:15 -0700

    Update financialTracker.js
    finished add transaction

--- FinancialOverview/financialTracker.js
+++ FinancialOverview/financialTracker.js
@@ -1,9 +1,44 @@
-class FinancialTraker{
+class FinancialTracker {
+  constructor() {
+    this.transactions = [];
+  }
+
   addTransaction() {
-    const name = prompt("Please enter transaction name:");
-    const amount = prompt("Please enter transaction amount:");
+    const name = prompt("Transaction Name:");
+    const amount = parseFloat(prompt("Enter Income or Expense (+ or -):"));
 
-    if(!name || amount) return;
+    this.transactions.push({ name, amount });
 
+    this.updateUI();
+  }
+
+  getBalance() {
+    let total = 0;
+
+    for (let i = 0; i < this.transactions.length; i++) {
+      total += this.transactions[i].amount;
+    }
+
+    return total;
+  }
+
+  updateUI() {
+    const list = document.getElementById("transactionList");
+    const balance = document.getElementById("balance");
+
+    list.innerHTML = "";
+
+    for (let i = 0; i < this.transactions.length; i++) {
+      const t = this.transactions[i];
+
+      const li = document.createElement("li");
+      li.textContent = t.name + ": $" + t.amount;
+
+      list.appendChild(li);
+    }
+
+    balance.textContent = "Total Balance:" + this.getBalance();
   }
 }
+
+const tracker = new FinancialTracker();
